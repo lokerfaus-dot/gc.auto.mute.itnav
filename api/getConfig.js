7597e0cfd63e9b3eca91de36dcac2ebbdde57a3f
lokerfaus-dot <lokerfaus@gmail.com>
2026-01-13 15:55:37 +0000

    old config

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     res.json({
-        GCclientId: "f65fd35a-4956-4b1c-89e7-a30d6aa65f78",//process.env.GC_OAUTH_CLIENT_ID,
-        DDIlist: "+972776936026"//process.env.DDI_LIST
+        GCclientId: process.env.GC_OAUTH_CLIENT_ID,
+        DDIlist: process.env.DDI_LIST
     });
 };
